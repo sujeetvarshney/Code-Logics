3696b960d345a6115cd5ed3b8d59dfcc0cb6feee
sujeet <sujeet.varshney@gmail.com>
2022-10-12 14:33:53 +0530

    second commit

---
+++ second.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout<<"enter 2 numbers ";
+	cin>>a>>b;
+	
+	if(a<b)
+	{
+		cout<<a<<" is smaller";
+	}
+	else
+	{
+		cout<<b<<" is smaller";
+	}
+	return 0;
+}
