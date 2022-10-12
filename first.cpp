facde9cf249cab46ddfd2286c970cc0ed2d2b0d1
sujeet <sujeet.varshney@gmail.com>
2022-10-12 14:14:47 +0530

    first program

---
+++ first.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout<<"enter 2 numbers ";
+	cin>>a>>b;
+	
+	if(a>b)
+	{
+		cout<<a<<" is bigger";
+	}
+	else
+	{
+		cout<<b<<" is bigger";
+	}
+	return 0;
+}
