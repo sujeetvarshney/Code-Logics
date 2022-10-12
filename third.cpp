a5018a685d47289e290e699680c762606b46fe16
sujeet <sujeet.varshney@gmail.com>
2022-10-12 17:34:25 +0530

    third commit

---
+++ third.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,i,t;
+	cout<<"enter a number ";
+	cin>>n;
+	
+	for(int i=1;i<=10;i++)
+	{
+		t=n*i;
+		
+		cout<<t<<endl;
+		
+	}
+	return 0;
+}
